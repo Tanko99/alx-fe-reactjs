fa1039a6406974215133814362528b30cb96492d
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-08-30 05:12:50 +0100

    welcome jsx add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+ import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <WelcmeMessage />
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
