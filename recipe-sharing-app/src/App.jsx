fd2a6ff496ea3ee5f4dda27a33ec61c798b45d2d
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-14 11:42:46 +0100

    add recipe and recipe list

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/Recipelist';
+
+const App = () => {
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+};
+
+export default App;
