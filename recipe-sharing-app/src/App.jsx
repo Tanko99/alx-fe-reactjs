e7ff07680b71991b5fdeffaf2c523f7a5bb12d11
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-15 08:51:14 +0100

    adding routing to the recipe app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/Recipelist';
 import RecipeDetails from './components/RecipeDeatils';
 
 
-function App () {
+function App ()  {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<RecipeList />} />
-      <Route path="/recipe/:id" element={<RecipeDetails />} />
+    <div>
+      <h1> Recipe Sharing App</h1>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="edit/:id" element={<EditRecipeForm />}  />
       </Routes>
-      </BrowserRouter>
-  );
+    </div>
+  )
 }
 
 
--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,16 @@
 import useRecipeStore from "./recipeStore";
 
 
-const DeleteRecipeButton = () => [
+const DeleteRecipeButton = ({reciprId}) => [
     const deleteRecipe = useRecipeStore(state=> state.deleteRecipe);
+    const handleDelete = () => {
+        if(windows,confirm("Are you sure you want to delete this recipe?")){
+            deleteRecipe(recipeId);
+        }
+    };
     return (
-        <button onClick={()=> deleteRecipe(recipeId)}>
-            Delete recipe
+        <button onClick={handleDelete}>
+            Delete 
         </button>
     );
 ];
