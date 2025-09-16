dee09bb8a827cb93eb8afb8afc8368b4e6be5e6f
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-16 15:29:38 +0100

    favorites and recommendations features added to app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,8 @@ import RecipeDetails from "./components/RecipeDetails";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar"; 
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
           <Route path="/recipes" element={<RecipeList />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
         </Routes>
       </BrowserRouter>
     </div>
