d325de6032ddb6d5277f3c4c297dc8c21a52c0c3
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-21 19:53:16 +0100

    setting up the react  for the github user search app

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,17 +1,19 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
+import SearchBar from './components/SearchBar';
+import UserProfile from './components/UserProfile';
 
 const App = () => {
     return (
-        <div>
-            <h1>Github User Search App</h1>
-            <BrowserRouter>
-            <Routes>
-                
-            </Routes>
-            </BrowserRouter>
-        </div>
+        <div> 
+            <h1 className="text-20xl text-center font-bold px-8 py-8"> Github User Search App</h1>
+       <BrowserRouter>
+           <Routes>
+            <Route path="/search" element={<SearchBar /> />
+            <Route path="/user/:id" element={<UserProfile />} />
+           </Routes>
+       </BrowserRouter>
+       </div>
     );
 };
 
