69e7f29b5de64ffb53dd10cc09fa674535d09bd7
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-21 14:46:32 +0100

    setting up the react project and configuring API calls environment

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Github User Search App</h1>
+            <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
