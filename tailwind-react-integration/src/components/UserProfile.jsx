e7061c992750ff1656225fd490bf32850a26b57d
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-25 11:26:06 +0100

    transitions and animations aded to user card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm mx-auto my-20  hover:shawdow-xl
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm mx-auto my-20  hover:shadow-xl
     max-w-xs rounded-lg shadow-lg">
-      <img className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24
-      hover:scale-110 transition-transform duration-200 ease-in-out" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className=" sm:text-lg md:text-xl hobbber:text-blue-500
+      <img className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24 hover:scale-110 transition-transform
+      duration-300 ease-in-out" src="https://via.placeholder.com/150" alt="User" />
+      <h1 className=" sm:text-lg md:text-xl hover:text-blue-500
       text-blue-800 sm:my-2 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
