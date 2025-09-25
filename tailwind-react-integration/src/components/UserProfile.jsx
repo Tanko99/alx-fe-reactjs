fd3d46adbb9eff81e86e6d414960693000ddec28
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-09-25 10:47:55 +0100

    Fixes for responsiveness in the container

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 sm:my-10 
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm mx-auto my-20 
     max-w-xs rounded-lg shadow-lg">
-      <img className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className=" sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <img className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24" src="https://via.placeholder.com/150" alt="User" />
+      <h1 className=" sm:text-lg md:text-xl text-blue-800 sm:my-2 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
