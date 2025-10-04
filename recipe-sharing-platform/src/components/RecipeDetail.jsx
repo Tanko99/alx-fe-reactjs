4d295d888a33b6d08463151312afb47a6d33e3be
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-10-04 08:48:45 +0100

    feat:recipe details fixed and frouting enhanced

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import data from "../data.json";
 
-const RecipeDetail = () => {
+function  RecipeDetail () {
   const { id } = useParams(); // get id from URL
   const recipeId = parseInt(id, 10);
   const recipe = data.find((r) => r.id === recipeId);
@@ -11,7 +11,7 @@ const RecipeDetail = () => {
   }
 
   return (
-    <div className="min-h-screen bg-green-400 p-6">
+    <div className="min-h-screen bg-green-400 sm:px-8 px-4 py-6">
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-6">
         <h1 className="md:text-3xl sm:text-lg text-xl font-bold mb-4">{recipe.title}</h1>
         <img
@@ -23,9 +23,9 @@ const RecipeDetail = () => {
 
         {/* Ingredients */}
         <div className="mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Ingredients</h2>
+          <h2 className="md:text-2xl sm:text-lg text-xl font-semibold mb-2">Ingredients</h2>
           <ul className="list-disc list-inside space-y-1">
-            {recipe.ingredients.map((ingredient, index) => (
+               {recipe.ingredients.map((ingredient, index) => (
               <li key={index} className="text-gray-600">
                 {ingredient}
               </li>
@@ -35,7 +35,7 @@ const RecipeDetail = () => {
 
         {/* Instructions */}
         <div>
-          <h2 className="text-2xl font-semibold mb-2">Instructions</h2>
+          <h2 className="md:text-2xl sm:::text-lg text-xl font-semibold mb-2">Instructions</h2>
           <ol className="list-decimal list-inside space-y-2">
             {recipe.instructions.map((instruction, index) => (
               <li key={index} className="text-gray-600">
@@ -47,6 +47,6 @@ const RecipeDetail = () => {
       </div>
     </div>
   );
-};
+}
 
 export default RecipeDetail;
